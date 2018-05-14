ec93b3c5be44f89ad2d09299487194f627cf0c7b
tinakuzmenko <35991982+tinakuzmenko@users.noreply.github.com>
2018-05-14 19:09:04 +0300

    added js-code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -14,23 +14,36 @@ function windowClose() {
 
 /* Catalogue filter function */
 
+var allItemsFilter = document.getElementById('all-items');
 var allItems = document.getElementsByClassName('catalogue-item');
-var adaptiveFilter = document.getElementsById('adaptive');
+var adaptiveFilter = document.getElementById('adaptive');
 var adaptiveItem = document.getElementsByClassName('adaptive-item');
-var responsiveFilter = document.getElementsById('responsive');
+var responsiveFilter = document.getElementById('responsive');
 var responsiveItem = document.getElementsByClassName('responsive-item');
-var fixedFilter = document.getElementsById('fixed');
+var fixedFilter = document.getElementById('fixed');
 var fixedItem = document.getElementsByClassName('fixed-item');
 
 function showSelected() {
-    allItems.style.display = "none";
-    if(adaptiveFilter.type === "radio" && adaptiveFilter.checked) {
-        adaptiveItem.style.display = "inline-block";
+    Object.values(allItems).map(function(element) {
+        element.style.display = 'none';
+    });
+    if (allItemsFilter.type === "radio" && allItemsFilter.checked) {
+        Object.values(allItems).map(function(element){
+            element.style.display = 'inline-block';
+        });
+    } else if(adaptiveFilter.type === "radio" && adaptiveFilter.checked) {
+        Object.values(adaptiveItem).map(function(element){
+            element.style.display = 'inline-block';
+        });
     } else if(responsiveFilter.type === "radio" && responsiveFilter.checked) {
-        responsiveItem.style.display = "inline-block";
+        Object.values(responsiveItem).map(function(element){
+            element.style.display = 'inline-block';
+        });
     } else if(fixedFilter.type === "radio" && fixedFilter.checked) {
-        fixedItem.style.display = "inline-block";
-    }
+        Object.values(fixedItem).map(function(element){
+            element.style.display = 'inline-block';
+        });
+    };
 };
 
 /* Catalogue sort function */
