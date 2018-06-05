360ec029246c5bb2932c7d2501fc23c813c161b8
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2018-06-05 10:08:13 +0200

    Filter script added

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -26,6 +26,36 @@ document.addEventListener('keypress', function(event) {
 
 /* Catalogue filter function */
 
+var allItemsFilter = document.getElementById('all-items');
+var allItems = document.getElementsByClassName('catalogue-item');
+var adaptiveFilter = document.getElementById('adaptive');
+var adaptiveItem = document.getElementsByClassName('adaptive-item');
+var responsiveFilter = document.getElementById('responsive');
+var responsiveItem = document.getElementsByClassName('responsive-item');
+var fixedFilter = document.getElementById('fixed');
+var fixedItem = document.getElementsByClassName('fixed-item');
 
+function showSelected() {
+    Object.values(allItems).map(function(element) {
+        element.style.display = 'none';
+    });
+    if (allItemsFilter.type === "radio" && allItemsFilter.checked) {
+        Object.values(allItems).map(function(element){
+            element.style.display = 'inline-block';
+        });
+    } else if(adaptiveFilter.type === "radio" && adaptiveFilter.checked) {
+        Object.values(adaptiveItem).map(function(element){
+            element.style.display = 'inline-block';
+        });
+    } else if(responsiveFilter.type === "radio" && responsiveFilter.checked) {
+        Object.values(responsiveItem).map(function(element){
+            element.style.display = 'inline-block';
+        });
+    } else if(fixedFilter.type === "radio" && fixedFilter.checked) {
+        Object.values(fixedItem).map(function(element){
+            element.style.display = 'inline-block';
+        });
+    };
+};
 
 /* Catalogue sort function */
